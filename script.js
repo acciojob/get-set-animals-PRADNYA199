27def40bf2dd32a291e599049a7eb354c10261ca
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-08-29 09:48:34 -0700

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Animal {
 		this.species=species;
 	}
 	makeSound(){
-		console.log("sound the animal makes");
+		console.log(`The ${this.species} makes a sound`);
 	}
 }
 
@@ -30,6 +30,12 @@ class Cat extends Animal {
 	}
 }
 
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
